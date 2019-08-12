cd4eacbb56d4421916bcea456d4b0386776e3eac
Alexander Niederbühl <a.niederbuehl@gmail.com>
2019-08-12 11:26:10 +0200

    Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import * as ROUTES from "./constants/routes";
+import Chat from "./components/Chat";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to={ROUTES.CHAT}>Chat</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+        </li>
+      </ul>
     </div>
-  );
-}
+
+    <Route path={ROUTES.CHAT} component={Chat} />
+    <Route path={ROUTES.SIGN_UP} component={SignUp} />
+    <Route path={ROUTES.SIGN_IN} component={SignIn} />
+  </Router>
+);
 
 export default App;
---
+++ src/constants/routes.ts
@@ -0,0 +1,3 @@
+export const CHAT = "/chat";
+export const SIGN_IN = "/signin";
+export const SIGN_UP = "/signup";
