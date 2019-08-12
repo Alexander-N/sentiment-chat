7b39c196c17ad420276e6e6414ce32ca77fb8a18
Alexander Niederbühl <a.niederbuehl@gmail.com>
2019-08-13 00:59:16 +0200

    Add sentiment analysis for every new message

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,29 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+const language = require("@google-cloud/language");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const client = new language.LanguageServiceClient();
+
+exports.addSentimentInfo = functions
+  .region("europe-west2")
+  .firestore.document("messages/{messageId}")
+  .onCreate(async (message, context) => {
+    const messageText = message.data()!.text;
+    console.log("Retrieved message content: ", messageText);
+
+    const document = {
+      content: messageText,
+      type: "PLAIN_TEXT"
+    };
+
+    const [result] = await client.analyzeSentiment({
+      document: document,
+      encodingType: "UTF8"
+    });
+    const sentiment = result.documentSentiment;
+
+    return message.ref.update({
+      sentiment: sentiment.score
+    });
+  });
